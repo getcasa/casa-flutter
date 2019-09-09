fb76ef21fcb313913c9feaa837d00ef2e1c6cd40
Jimi M <jimi.maiquez@gmail.com>
2019-09-09 15:31:53 +0200

    organize files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'signin.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,123 +13,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Color.fromRGBO(255, 177, 66, 1),
         accentColor: Color.fromRGBO(255, 177, 66, 1),
       ),
-      home: MyCustomForm(),
-    );
-  }
-}
-
-// Define a custom Form widget.
-class MyCustomForm extends StatefulWidget {
-  @override
-  _MyCustomFormState createState() => _MyCustomFormState();
-}
-
-// Define a corresponding State class.
-// This class holds the data related to the Form.
-class _MyCustomFormState extends State<MyCustomForm> {
-  // Create a text controller and use it to retrieve the current value
-  // of the TextField.
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    emailController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: 20.0,
-          right: 20.0
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Welcome\nto',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.only(bottom: 30.0),
-              child: Text(
-                'Casa',
-                style: TextStyle(fontSize: 50, color: Theme.of(context).accentColor),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 20.0),
-              child: Material(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextField(
-                    controller: emailController,
-                    autofocus: true,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                elevation: 20.0,
-                shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 20.0),
-              child: Material(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                elevation: 20.0,
-                shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              child: Container(
-                width: 60.0,
-                height: 60.0,
-                child: Material(
-                  child: FlatButton(
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.keyboard_arrow_right,
-                      color: Colors.white,
-                      size: 30.0,
-                    ),
-                    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
-                  ),
-                  color: Theme.of(context).accentColor,
-                  shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
-                  elevation: 20.0,
-                  shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+      home: SignInPage(),
     );
   }
 }
---
+++ lib/signin.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+// Define a custom Form widget.
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _SignInPageState extends State<SignInPage> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: 20.0,
+          right: 20.0
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Welcome\nto',
+                style: TextStyle(fontSize: 50),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(bottom: 30.0),
+              child: Text(
+                'Casa',
+                style: TextStyle(fontSize: 50, color: Theme.of(context).accentColor),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20.0),
+              child: Material(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: emailController,
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                elevation: 20.0,
+                shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20.0),
+              child: Material(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                elevation: 20.0,
+                shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              child: Container(
+                width: 60.0,
+                height: 60.0,
+                child: Material(
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
+                    },
+                    child: Icon(
+                      Icons.keyboard_arrow_right,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
+                  ),
+                  color: Theme.of(context).accentColor,
+                  shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
+                  elevation: 20.0,
+                  shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
