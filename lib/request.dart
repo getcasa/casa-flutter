8d5c8904b0652b9e20d959f93ab8360d155ae2b3
Jimi M <jimi.maiquez@gmail.com>
2019-09-18 16:02:00 +0200

    move http requests in one class

---
+++ lib/request.dart
@@ -0,0 +1,37 @@
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+import 'dart:convert';
+
+class Request {
+  SharedPreferences prefs;
+  String token;
+  String apiUrl = 'http://192.168.1.46:3000';
+
+  Request() {
+    SharedPreferences.getInstance().then((_prefs) {
+      prefs = _prefs;
+
+      token = prefs.getString('token');
+    });
+  }
+
+  Future<dynamic> getHomes() async {
+    var response = await http.get(
+      apiUrl + '/v1/homes',
+      headers: {HttpHeaders.authorizationHeader: 'Bearer '+token},
+    );
+    var parsedJson = json.decode(response.body);
+    return parsedJson;
+  }
+
+  Future<dynamic> addHome(body) async {
+    var response = await http.post(
+      apiUrl + '/v1/homes',
+      headers: {HttpHeaders.authorizationHeader: 'Bearer '+token},
+      body: body
+    );
+    var parsedJson = json.decode(response.body);
+    return parsedJson;
+  }
+}
