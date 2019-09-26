0736f0757fe830d94c861d5c9e9893e35baff82c
Jimi M <jimi.maiquez@gmail.com>
2019-09-26 16:22:37 +0200

    add tabs to rooms page

--- lib/rooms.dart
+++ lib/rooms.dart
@@ -14,21 +14,16 @@ class RoomsPage extends StatefulWidget {
 class _RoomsPageState extends State<RoomsPage> {
   Request request = new Request();
   Dialogs dialogs = new Dialogs();
-  List<dynamic> homes;
-  int homeIndex = 0;
-  String homeName = '';
+  List<dynamic> rooms;
+  String roomName = '';
 
   @override
   void initState() {
     super.initState();
     request.init().then((_token) {
-      request.getHomes().then((_homes) {
+      _getRooms().then((_rooms) {
         setState(() {
-          homes = _homes['data'];
-          if (widget.homeId != '') {
-            homeIndex = homes.indexWhere((_home) => _home['id'] == widget.homeId);
-          }
-          homeName = homes[homeIndex]['name'];
+          rooms = _rooms['data'];
         });
       });
       return;
@@ -36,13 +31,13 @@ class _RoomsPageState extends State<RoomsPage> {
   }
 
   Future<Null> addRoom(String name) {
-    return request.addRoom(homes[homeIndex]['id'], { 'name': name }).then((room) {
+    return request.addRoom(widget.homeId, { 'name': name }).then((room) {
       print(room);
     });
   }
 
   Future<dynamic> _getRooms() async {
-    var rooms = await request.getRooms(homes[homeIndex]['id']);
+    var rooms = await request.getRooms(widget.homeId);
     return rooms;
   }
 
@@ -52,7 +47,12 @@ class _RoomsPageState extends State<RoomsPage> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0,
-        title: Text(homeName),
+        leading: IconButton(
+          icon: Icon(Icons.list, color: Colors.black),
+          onPressed: () {
+            print('Rooms list');
+          },
+        ),
         actions: <Widget>[
           PopupMenuButton(
             onSelected: (select) {
@@ -82,81 +82,60 @@ class _RoomsPageState extends State<RoomsPage> {
           ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0),
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Rooms',
-              style: TextStyle(
-                decoration: TextDecoration.underline,
-                decorationColor: Theme.of(context).accentColor,
-                fontSize: 18
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: FutureBuilder(
-              future: _getRooms(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                // print(snapshot);
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child: CircularProgressIndicator()
-                  );
-                }
-                if (snapshot.data == null) {
-                  return Text('No rooms');
-                }
-                return Container(
-                  height: double.infinity,
-                  padding: EdgeInsets.all(0.0),
-                  child: GridView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data['data'].length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2
-                    ),
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        padding: EdgeInsets.all(20.0),
-                        child: Material(
-                          child: FlatButton(
-                            onPressed: () {},
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.home,
-                                  color: Colors.black,
-                                  size: 50.0,
-                                ),
-                                Text(
-                                  snapshot.data['data'][index]['name'],
-                                  textAlign: TextAlign.center,
-                                )
-                              ]
-                            ),
-                            shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
-                          ),
-                          color: Colors.white,
-                          shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
-                          elevation: 20.0,
-                          shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
+      body: rooms == null || rooms.length == 0
+        ? Center(
+          child: CircularProgressIndicator(),
+        )
+        : DefaultTabController(
+          length: rooms.length,
+          child: TabBarView(
+            children: rooms.map((room) {
+              return Container(
+                padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        room['name'],
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
-                      );
-                    }
-                  )
-                );
-              },
-            ),
-          )
-        ],
-      ),
-      bottomNavigationBar: BottomNavigation(1, homes[homeIndex]['id'])
+                      )
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 40.0),
+                      alignment: Alignment.centerLeft,
+                      child: Text.rich(
+                        TextSpan(
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Dev',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                decorationColor: Theme.of(context).accentColor
+                              )
+                            ),
+                            TextSpan(
+                              text: 'ices'
+                            ),
+                          ],
+                        ),
+                      )
+                    )
+                  ],
+                )
+              );
+            }).toList()
+          ),
+        ),
+      bottomNavigationBar: BottomNavigation(1, widget.homeId)
     );
   }
 }
