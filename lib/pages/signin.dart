ccd407e70bbc2bd8fd75a2b0a14966e61adf217c
Jimi M <jimi.maiquez@gmail.com>
2019-10-28 17:57:43 +0100

    add link to signup page on signin page

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,8 +1,8 @@
+import 'package:casa/pages/signup.dart';
 import 'package:casa/pages/splash_screen.dart';
 import 'package:casa/request.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'home.dart';
 
 // Define a custom Form widget.
 class SignInPage extends StatefulWidget {
@@ -105,42 +105,64 @@ class _SignInPageState extends State<SignInPage> {
                 borderRadius: BorderRadius.all(Radius.circular(8.0)),
               ),
             ),
-            Container(
-              alignment: Alignment.centerRight,
-              child: Container(
-                width: 60.0,
-                height: 60.0,
-                child: Material(
-                  child: FlatButton(
-                    onPressed: () async {
-                      var response;
-                      try {
-                        response = await request.signin(emailController.text, passwordController.text);
-                      } catch (e) {
-                        final snackBar = SnackBar(content: Text(e));
-                        _scaffoldKey.currentState.showSnackBar(snackBar);
-                        return;
-                      }
-
-                      await prefs.setString('token', response['message']);
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => SplashScreen()),
-                      );
-                    },
-                    child: Icon(
-                      Icons.keyboard_arrow_right,
-                      color: Colors.white,
-                      size: 30.0,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpPage()),
+                    );
+                  },
+                  child: Container(
+                    child: Text(
+                      "You don't have an account?",
+                      style: TextStyle(
+                        color: Theme.of(context).accentColor,
+                        decoration: TextDecoration.underline,
+                      ),
                     ),
-                    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
-                  ),
-                  color: Theme.of(context).accentColor,
-                  shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
-                  elevation: 20.0,
-                  shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
+                  )
                 ),
-              ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    child: Material(
+                      child: FlatButton(
+                        onPressed: () async {
+                          var response;
+                          try {
+                            response = await request.signin(emailController.text, passwordController.text);
+                          } catch (e) {
+                            final snackBar = SnackBar(content: Text(e));
+                            _scaffoldKey.currentState.showSnackBar(snackBar);
+                            return;
+                          }
+
+                          await prefs.setString('token', response['message']);
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => SplashScreen()),
+                          );
+                        },
+                        child: Icon(
+                          Icons.keyboard_arrow_right,
+                          color: Colors.white,
+                          size: 30.0,
+                        ),
+                        shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
+                      ),
+                      color: Theme.of(context).accentColor,
+                      shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
+                      elevation: 20.0,
+                      shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
+                    ),
+                  ),
+                )
+              ],
             )
           ],
         ),
