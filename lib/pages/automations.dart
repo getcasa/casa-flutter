9dc0f4c6b0fa6fcd1cede1768609be58546a24aa
Jimi M <jimi.maiquez@gmail.com>
2019-11-20 15:46:58 +0100

    fix: automations layout

--- lib/pages/automations.dart
+++ lib/pages/automations.dart
@@ -1,3 +1,5 @@
+import 'package:casa/components/bottom_navigation.dart';
+import 'package:casa/components/styled_components.dart';
 import 'package:flutter/material.dart';
 import 'package:casa/request.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -12,6 +14,7 @@ class AutomationsPage extends StatefulWidget {
 
 class _AutomationsPageState extends State<AutomationsPage> {
   Request request = new Request();
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = new GlobalKey<RefreshIndicatorState>();
 
   @override
   void initState() {
@@ -28,32 +31,63 @@ class _AutomationsPageState extends State<AutomationsPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        elevation: 0.0
-      ),
-      body: FutureBuilder(
-        future: _getAutomations(),
-        builder: (context, projectSnap) {
-          if (projectSnap == null || projectSnap.data == null) {
-            return CircularProgressIndicator();
-          }
-          return ListView.builder(
-            itemCount: projectSnap.data.length,
-            itemBuilder: (context, index) {
-              var automation = projectSnap.data[index];
-              return Container(
-                margin: EdgeInsets.only(bottom: 5),
-                child: ListTile(
-                  title: Text(automation['name']),
-                  onTap: () {
-                    print(automation);
-                  },
-                  trailing: Icon(MdiIcons.arrowRight),
-                )
-              );
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              MdiIcons.plus,
+            ),
+            onPressed: () {
+              print('add');
             },
-          );
-        },
+          ),
+        ],
       ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(right: 20, left: 20),
+            child: StyledTitle('Aut_omations')
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.only(top: 5),
+              height: double.infinity,
+              child: FutureBuilder(
+                future: _getAutomations(),
+                builder: (context, projectSnap) {
+                  if (projectSnap == null || projectSnap.data == null) {
+                    return CircularProgressIndicator();
+                  }
+                  return RefreshIndicator(
+                    key: _refreshIndicatorKey,
+                    onRefresh: _getAutomations,
+                    child: ListView.builder(
+                      itemCount: projectSnap.data.length,
+                      itemBuilder: (context, index) {
+                        var automation = projectSnap.data[index];
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 5),
+                          child: ListTile(
+                            title: Text(automation['name']),
+                            onTap: () {
+                              print(automation);
+                            },
+                            trailing: Icon(MdiIcons.arrowRight),
+                          )
+                        );
+                      },
+                    )
+                  );
+                },
+              )
+            )
+          )
+        ]
+      ),
+      bottomNavigationBar: BottomNavigation(2, widget.homeId)
     );
   }
 }
