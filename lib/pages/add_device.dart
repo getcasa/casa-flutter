03e9179b93d1a854f0d2dbc1099850a812b361fa
Alex <amoinier@outlook.fr>
2019-12-04 11:03:07 +0100

    add searchbar for icon added devices

--- lib/pages/add_device.dart
+++ lib/pages/add_device.dart
@@ -37,46 +37,65 @@ class _AddDevicePageState extends State<AddDevicePage> {
       });
     };
 
+    var changeIconList = (String value) {
+      icons =  MdiIcons.getIconsName().where((elem) => elem.contains(value)).toList();
+    };
+
     return showModalBottomSheet<void>(
       context: context,
       builder: (BuildContext context) {
-        return StatefulBuilder(
-          builder: (BuildContext context, StateSetter setModalState) {
-            return GridView.builder(
-              padding: EdgeInsets.all(20.0),
-              shrinkWrap: true,
-              itemCount: icons.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                childAspectRatio: 1,
-                crossAxisSpacing: 20.0,
-                mainAxisSpacing: 20.0
+        return Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Find an icon...'
+                ),
+                onChanged: (String value) {
+                  changeIconList(value);
+                },
               ),
-              itemBuilder: (BuildContext context, int index) {
-                var icon = MdiIcons.fromString(icons[index]);
-                var isSelected = icons[index] == selectedIcon;
+            ),
+            Flexible(
+              flex: 1,
+              child: GridView.builder(
+                padding: EdgeInsets.all(20.0),
+                shrinkWrap: true,
+                itemCount: icons.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  childAspectRatio: 1,
+                  crossAxisSpacing: 20.0,
+                  mainAxisSpacing: 20.0
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  var icon = MdiIcons.fromString(icons[index]);
+                  var isSelected = icons[index] == selectedIcon;
 
-                return Material(
-                  child: FlatButton(
-                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                    onPressed: () {
-                      updateIcon(icons[index]);
-                      Navigator.pop(context);
-                    },
-                    child: Icon(
-                      icon,
-                      color: isSelected ? Colors.white : Colors.black,
+                  return Material(
+                    child: FlatButton(
+                      padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                      onPressed: () {
+                        updateIcon(icons[index]);
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        icon,
+                        color: isSelected ? Colors.white : Colors.black,
+                      ),
+                      shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
                     ),
                     shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
-                  ),
-                  shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),
-                  elevation: 20.0,
-                  shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
-                  color: isSelected ? Theme.of(context).accentColor : Colors.white,
-                );
-              },
-            );
-          }
+                    elevation: 20.0,
+                    shadowColor: Color.fromRGBO(0, 0, 0, 0.4),
+                    color: isSelected ? Theme.of(context).accentColor : Colors.white,
+                  );
+                },
+              )
+            )
+          ]
         );
       },
       shape: RoundedRectangleBorder(
